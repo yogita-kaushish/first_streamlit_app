54523dbc8835142e3639389fd2b2ba0f0013a593
Yogita Kaushish <contact@yogitakaushish.com>
2024-02-14 15:34:40 +0530

    Update test_file.py

--- test_file.py
+++ test_file.py
@@ -1,8 +1,22 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
+import pandas
+streamlit.title('My Mom's New Healthy Diner')
 
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favourites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Avocado Toast')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
+streamlit.dataframe(my_fruit_list)
