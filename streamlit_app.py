f0c650ea304cb4975631a548579f1245b8ccc5cc
Yogita Kaushish <contact@yogitakaushish.com>
2024-02-14 15:04:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,2 @@
 import streamlit
-import pandas
-streamlit.title('streamlit test file')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-
-
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
-
-# Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-
-
-my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.title('test app')
